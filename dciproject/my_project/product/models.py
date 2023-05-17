baabc773054d868bedb5448273df2dd4d30218e9
alj-b <aljoscha_b@icloud.com>
2023-05-17 11:58:24 +0200

    Created Product model

--- dciproject/my_project/product/models.py
+++ dciproject/my_project/product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200)
+    product_id = models.AutoField(primary_key=True)
+    product_category = models.CharField(max_length=200)
+    product_description = models.CharField(max_length=2000)
+    product_image_url = models.CharField(max_length=200)
+    product_price = models.FloatField()
+
+    
+    def __str__(self):
+        return self.product_name
