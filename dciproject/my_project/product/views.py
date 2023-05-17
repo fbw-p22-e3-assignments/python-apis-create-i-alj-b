b9ffe17ca9016ae84f62fb09342bc75162c36094
alj-b <aljoscha_b@icloud.com>
2023-05-17 15:11:21 +0200

    First attempt at GET products by name

--- dciproject/my_project/product/views.py
+++ dciproject/my_project/product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Product
 
@@ -7,3 +7,11 @@ from .models import Product
 class ProductListView(ListView):
     model = Product
     context_object_name = 'products'
+
+class ProductFilteredListView(ListView):
+    context_object_name = "products"
+    # queryset = Product.objects.filter(product_name=self.kwargs['name'])
+    
+    def get_queryset(self):
+        self.name = self.kwargs['name']
+        return Product.objects.filter(product_name=self.name)
