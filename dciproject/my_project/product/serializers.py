29426774a67a04027407f0b6f87acd7a8c38ffcb
alj-b <aljoscha_b@icloud.com>
2023-05-17 13:53:42 +0200

    Created ProductSerializer

--- dciproject/my_project/product/serializers.py
+++ dciproject/my_project/product/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['product_name', 'product_category', 'product_description', 'product_image_url', 'product_price']
