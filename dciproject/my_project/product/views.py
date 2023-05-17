28f40c8577215037d1081202c2869f43d3a71c76
alj-b <aljoscha_b@icloud.com>
2023-05-17 15:23:05 +0200

    Included template name in views

--- dciproject/my_project/product/views.py
+++ dciproject/my_project/product/views.py
@@ -6,12 +6,14 @@ from .models import Product
 
 class ProductListView(ListView):
     model = Product
+    template_name = 'product/product_list.html'
     context_object_name = 'products'
 
 class ProductFilteredListView(ListView):
-    context_object_name = "products"
+    context_object_name = 'products'
+    template_name = 'product/product_name_list.html'
     # queryset = Product.objects.filter(product_name=self.kwargs['name'])
-    
+
     def get_queryset(self):
         self.name = self.kwargs['name']
         return Product.objects.filter(product_name=self.name)
