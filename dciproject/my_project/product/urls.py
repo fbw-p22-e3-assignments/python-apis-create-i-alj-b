ed6e16a42893e4d263582be92ac552696344f87b
alj-b <aljoscha_b@icloud.com>
2023-05-17 16:15:30 +0200

    Realised that I should be working with Django Rest Framework Generic views, and not regular Django Generic views.

--- dciproject/my_project/product/urls.py
+++ dciproject/my_project/product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include 
-from .views import ProductListView, ProductFilteredListView
+from .views import ProductListAPIView, ProductRetrieveAPIView
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='overview'),
-    path('<name>/', ProductFilteredListView.as_view(), name='name_overview'), 
+    path('', ProductListAPIView.as_view(), name='api_list'),
+    path('<product_name>/', ProductRetrieveAPIView.as_view(), name='api_retrieve'), 
 ]
--- dciproject/my_project/product/views.py
+++ dciproject/my_project/product/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+
+# class UserList(generics.ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     permission_classes = [IsAdminUser]
 
 # Create your views here.
 
-class ProductListView(ListView):
-    model = Product
-    template_name = 'product/product_list.html'
-    context_object_name = 'products'
+class ProductListAPIView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-class ProductFilteredDetailView(DetailView):
-    context_object_name = 'product'
-    template_name = 'product/product_detail.html'
+class ProductRetrieveAPIView(RetrieveAPIView):
+    # context_object_name = 'product'
+    # template_name = 'product/product_detail.html'
     # queryset = Product.objects.filter(product_name=self.kwargs['name'])
+    queryset = Product.objects.all()
+    lookup_field = 'product_name'
+    serializer_class = ProductSerializer
 
-    def get_queryset(self):
-        self.name = self.kwargs['name']
-        return Product.objects.filter(product_name=self.name)
+    # def get_queryset(self):
+    #     self.name = self.kwargs['name']
+    #     return Product.objects.filter(product_name=self.name)
