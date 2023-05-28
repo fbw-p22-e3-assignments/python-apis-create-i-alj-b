12e7b86c411fa45d27bfec2bd275e685990d1fab
alj-b <aljoscha_b@icloud.com>
2023-05-28 17:12:38 +0200

    Removed unnecessary code snippets

--- dciproject/my_project/product/views.py
+++ dciproject/my_project/product/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView
@@ -24,13 +23,6 @@ class ProductListAPIView(ListAPIView):
     serializer_class = ProductSerializer
 
 class ProductRetrieveAPIView(RetrieveAPIView):
-    # context_object_name = 'product'
-    # template_name = 'product/product_detail.html'
-    # queryset = Product.objects.filter(product_name=self.kwargs['name'])
     queryset = Product.objects.all()
     lookup_field = 'product_name'
     serializer_class = ProductSerializer
-
-    # def get_queryset(self):
-    #     self.name = self.kwargs['name']
-    #     return Product.objects.filter(product_name=self.name)
